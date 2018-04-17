1223aef75187b12d4c78fb1e05e93c1effde4487
jose.sanpedro <jose.sanpedro@mediaportal.com.br>
2018-04-17 18:42:10 -0300

    testing profiles

--- src/main/java/br/com/tsp/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/tsp/api/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,26 @@
 package br.com.tsp.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Meu primeiro projeto"); 
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunnter() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
+		};	
 	}
 }
