e9f817f5c892b05eea1945d8f008fc8846d19666
jose.sanpedro <jose.sanpedro@mediaportal.com.br>
2018-04-19 17:36:40 -0300

    acrescentando DTO, Response e Doc Swagger

--- src/main/java/br/com/tsp/api/controllers/EmpresaController.java
+++ src/main/java/br/com/tsp/api/controllers/EmpresaController.java
@@ -1,9 +1,18 @@
 package br.com.tsp.api.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tsp.api.dtos.EmpresaDto;
+import br.com.tsp.api.responses.Response;
 
 @RestController
 @RequestMapping("/api/empresa")
@@ -12,5 +21,20 @@ public class EmpresaController {
 	@GetMapping(value = "/{nome}")
 	public String helloName(@PathVariable("nome") String nome) {
 		return "Olá " + nome;
+	}
+	
+	@PostMapping ResponseEntity<Response<EmpresaDto>> cadastrar(@Valid @RequestBody EmpresaDto empresaDto, 
+			BindingResult result) {
+		Response<EmpresaDto> response = new Response<>();
+		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		empresaDto.setId(1L);
+		response.setData(empresaDto);
+		
+		return ResponseEntity.ok(response);
 	}
 }
--- src/main/resources/db/migration/mysql/V2__tabela_empresa.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `empresa` (`razao_social`, `data_criacao`, `data_atualizacao`)
-VALUES ('Empresa Admin', CURRENT_DATE(), CURRENT_DATE());
